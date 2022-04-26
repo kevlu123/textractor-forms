ac6207a8c114e552778ce0a87b372108002f3697
kevlu123 <kevlu321@gmail.com>
2022-04-26 20:20:50 +1200

    Added icon to exe file and version number to title

--- TextractorForms/MainForm.Designer.cs
+++ TextractorForms/MainForm.Designer.cs
@@ -204,7 +204,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(595, 418);
             this.Name = "MainForm";
-            this.Text = "TextractorForms";
+            this.Text = "TextractorForms v1.0";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.OnFormClosed);
             this.ClientSizeChanged += new System.EventHandler(this.OnResize);
             this.ResumeLayout(false);
